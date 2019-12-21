3d5abb267c01dcd166a7dfc5a6f2578a1de6cc1b
mistervii <ouaaqil.yo@gmail.com>
2019-12-21 19:58:55 +0000

    update sql

--- public/myfrigo.sql
+++ public/myfrigo.sql
@@ -1,1 +1,269 @@
+-- MySQL dump 10.13  Distrib 5.7.28, for Linux (x86_64)
+--
+-- Host: localhost    Database: myfrigo
+-- ------------------------------------------------------
+-- Server version	5.7.28-0ubuntu0.18.04.4
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `customers`
+--
+
+DROP TABLE IF EXISTS `customers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `customers` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `state` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `customers`
+--
+
+LOCK TABLES `customers` WRITE;
+/*!40000 ALTER TABLE `customers` DISABLE KEYS */;
+INSERT INTO `customers` VALUES (2,'Sam','Smith','333-333-3333','ssmith@yahoo.com','33 Birch Rd','Miami','FL'),(3,'Brad','Traversy','333-333-3333','brad@test.com','333 South st','Portland','ME');
+/*!40000 ALTER TABLE `customers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `frigo`
+--
+
+DROP TABLE IF EXISTS `frigo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `frigo` (
+  `id_user` int(11) NOT NULL,
+  `id_ingrd` int(11) NOT NULL,
+  `quantite` int(11) NOT NULL,
+  `id_unite` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `frigo`
+--
+
+LOCK TABLES `frigo` WRITE;
+/*!40000 ALTER TABLE `frigo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `frigo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ingredients`
+--
+
+DROP TABLE IF EXISTS `ingredients`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ingredients` (
+  `id_ingrd` int(11) NOT NULL AUTO_INCREMENT,
+  `label_ingrd` varchar(100) NOT NULL,
+  PRIMARY KEY (`id_ingrd`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ingredients`
+--
+
+LOCK TABLES `ingredients` WRITE;
+/*!40000 ALTER TABLE `ingredients` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ingredients` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `recette_info`
+--
+
+DROP TABLE IF EXISTS `recette_info`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `recette_info` (
+  `id_recette` int(11) NOT NULL AUTO_INCREMENT,
+  `titre_recette` varchar(100) NOT NULL,
+  `temps_recette` int(11) NOT NULL,
+  `difficulte_recette` int(11) NOT NULL,
+  `reviews_recette` int(11) NOT NULL,
+  `image_recette` varchar(100) NOT NULL,
+  PRIMARY KEY (`id_recette`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `recette_info`
+--
+
+LOCK TABLES `recette_info` WRITE;
+/*!40000 ALTER TABLE `recette_info` DISABLE KEYS */;
+/*!40000 ALTER TABLE `recette_info` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `recette_ingrd`
+--
+
+DROP TABLE IF EXISTS `recette_ingrd`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `recette_ingrd` (
+  `id_recette` int(11) NOT NULL,
+  `id_ingrd` int(11) NOT NULL,
+  `quantite` int(11) NOT NULL,
+  `id_unite` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `recette_ingrd`
+--
+
+LOCK TABLES `recette_ingrd` WRITE;
+/*!40000 ALTER TABLE `recette_ingrd` DISABLE KEYS */;
+/*!40000 ALTER TABLE `recette_ingrd` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `recette_step`
+--
+
+DROP TABLE IF EXISTS `recette_step`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `recette_step` (
+  `id_recette` int(11) NOT NULL,
+  `step_no` int(11) NOT NULL,
+  `desc` varchar(600) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `recette_step`
+--
+
+LOCK TABLES `recette_step` WRITE;
+/*!40000 ALTER TABLE `recette_step` DISABLE KEYS */;
+/*!40000 ALTER TABLE `recette_step` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `regime`
+--
+
+DROP TABLE IF EXISTS `regime`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `regime` (
+  `id_regime` int(11) NOT NULL AUTO_INCREMENT,
+  `label_regime` varchar(25) NOT NULL,
+  PRIMARY KEY (`id_regime`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `regime`
+--
+
+LOCK TABLES `regime` WRITE;
+/*!40000 ALTER TABLE `regime` DISABLE KEYS */;
+/*!40000 ALTER TABLE `regime` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `regime_recette`
+--
+
+DROP TABLE IF EXISTS `regime_recette`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `regime_recette` (
+  `id_regime` int(11) NOT NULL,
+  `id_recette` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `regime_recette`
+--
+
+LOCK TABLES `regime_recette` WRITE;
+/*!40000 ALTER TABLE `regime_recette` DISABLE KEYS */;
+/*!40000 ALTER TABLE `regime_recette` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `unite`
+--
+
+DROP TABLE IF EXISTS `unite`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `unite` (
+  `id_unite` int(11) NOT NULL,
+  `label` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `unite`
+--
+
+LOCK TABLES `unite` WRITE;
+/*!40000 ALTER TABLE `unite` DISABLE KEYS */;
+/*!40000 ALTER TABLE `unite` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(25) NOT NULL,
+  `id_regime` int(11) NOT NULL,
+  PRIMARY KEY (`id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-12-21 19:55:11
