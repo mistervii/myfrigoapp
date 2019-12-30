d53d90bd6ba816b5c87a7365b085369a0d640323
youssef <ouaaqil.yo@gmail.com>
2019-12-30 23:29:19 +0100

    Update myfrigo.php

--- public/myfrigo.php
+++ public/myfrigo.php
@@ -16,7 +16,11 @@ $app->get('/api/help', function(Request $request, Response $response){
  GET: /api/recipe/ingrd/{id_recette} => ingredients d une recette\r\n ;
  GET: /api/recipe/steps/{id_recette} => etapes d une recette\r\n ;
  GET: /api/frigo_recipes/{id_user} => listes recette suggere\r\n ;
- PUT: /api/frigo/update/{id_user} => mise a jour du frigo d un user ps: ajouter les parametres id_ingrd , quantite , id_unite\n");
+ PUT: /api/frigo/update/{id_user} => mise a jour du frigo d un user ps: ajouter les parametres id_ingrd , quantite , id_unite\n
+ GET: /api/users => liste users
+ GET: /api/user/{id} => l utilisateur avec id
+ 
+ ");
    
    
    
