46395be4e9cdebab7416d6cb66763cf6bd868e39
youssef <ouaaqil.yo@gmail.com>
2019-12-31 02:21:57 +0100

    Update myfrigo.php

--- public/myfrigo.php
+++ public/myfrigo.php
@@ -42,7 +42,7 @@ $app->get('/api/users', function(Request $request, Response $response){
         $stmt = $db->query($sql);
         $users = $stmt->fetchAll(PDO::FETCH_OBJ);
         $db = null;
-       $response = $response->getBody()->write($users);
+       $response = $response->getBody()->write( json_encode($users));
 //
        return $response;
     } catch(PDOException $e){
